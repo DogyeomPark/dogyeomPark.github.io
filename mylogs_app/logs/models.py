23d2a10e873955e02b6dadbaed7271e00380bea6
DogyeomPark <qkrehfud@gmail.com>
2025-03-17 12:06:38 +0900

    create User profile model

--- mylogs_app/logs/models.py
+++ mylogs_app/logs/models.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Userprofile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
 class DailySummary(models.Model):
     summary = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
